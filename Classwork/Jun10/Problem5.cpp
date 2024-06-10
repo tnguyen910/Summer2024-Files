3b8e78201a25a27c55a781187c85002168b21c4e
tnguyen910 <tnguyen910@ivc.edu>
2024-06-10 17:22:03 +0000

    Problem5

--- Classwork/Jun10/Problem5.cpp
+++ Classwork/Jun10/Problem5.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "What is the temperature in Fahrenheit: ";
     std::cin >> degreesFahrenheit;
 
-    float degreesCelcius = (degreesFahrenheit-32) * (5/9);
+    float degreesCelcius = (degreesFahrenheit-32) * (5.0/9);
     std::cout << degreesCelcius << std::endl;
     return 0;
 }
