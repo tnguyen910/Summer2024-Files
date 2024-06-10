d86e372eefdf6e257a9655c01eeb6e039fc768e6
tnguyen910 <tnguyen910@ivc.edu>
2024-06-10 17:19:12 +0000

    Problem 4

--- Classwork/Jun10/Problem4.cpp
+++ Classwork/Jun10/Problem4.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-
-    float r;
+    double r;
     std::cout << "What is the radius of your sphere: ";
-    float volume = (4/3)*3.14*r*r*r;
+    std::cin >> r;
+    double volume = 4.0/3.0 * M_PI * (r*r*r);
     std::cout << volume << std::endl;
     return 0;
 }
