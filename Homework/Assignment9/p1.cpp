7045bbb1c3529fe4941c2703f1d6ee4218217a38
tim-nguyen0 <54385326+tim-nguyen0@users.noreply.github.com>
2024-07-11 09:15:44 -0700

    update

---
+++ Homework/Assignment9/p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//Floyd's Triangle
+int main() {
+    int n = 4;
+    cin >> n;
+    cout << "hello";
+    for (int i = 0; i < n; i++) {
+        int j;
+        for (int k = 0; k < i; k++){
+            cout << j << " ";
+            j++;
+        }
+        cout << endl;
+    }
+    while(true){
+        continue;
+    }
+    return 0;
+}
