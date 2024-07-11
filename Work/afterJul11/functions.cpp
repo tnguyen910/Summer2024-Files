74b151447ae6146c8f7e6adb3baaffdb11cd3dff
tim-nguyen0 <54385326+tim-nguyen0@users.noreply.github.com>
2024-07-11 10:45:54 -0700

    update

---
+++ Work/afterJul11/functions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "functions.h"
+
+class utilityFunctions {
+
+};
+
+class problems {
+
+};
---
+++ Work/afterJul11/functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <iostream>
+#include <vector>
+#include <string>
+
+//Add Functions
+class utilityFunctions {
+
+};
+
+class problems {
+
+};
+
+#endif
