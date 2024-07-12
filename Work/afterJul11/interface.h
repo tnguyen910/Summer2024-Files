b295d6c8d3c3cb1e7e2d3adb90b5cdb73bd24ee7
Tim Nguyen <tim@Tims-MacBook-Pro.local>
2024-07-11 22:57:15 -0700

    updating interface

--- Work/afterJul11/interface.h
+++ Work/afterJul11/interface.h
@@ -1,12 +1,13 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef INTERFACE_H
+#define INTERFACE_H
 #include <iostream>
 #include <vector>
 #include <string>
 
 //Add Functions
 class utilityFunctions {
-
+    public:
+    int impl(std::string test);
 };
 
 class problems {
--- Work/afterJul11/main.cpp
+++ Work/afterJul11/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "functions.h"
+#include "interface.h"
 using namespace std;
 
 int main() {
