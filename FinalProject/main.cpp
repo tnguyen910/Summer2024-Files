04c267160c6fd0371c654dde0be8ccb0de4f972d
tim-nguyen0 <54385326+tim-nguyen0@users.noreply.github.com>
2024-07-20 01:41:42 -0700

    game start

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -2,9 +2,39 @@
 #include "functions.h"
 
 int main() {
+
+    std::cout << "CONSOLE CHESS GAME: " << std::endl;
+    std::cout << "View Chess Symbols? (Y/N): ";
+
+    bool symbols = true;
+    while (true){
+        char symbolsIn;
+        std::cin >> symbolsIn;
+
+        if (symbolsIn == 'Y') {
+            symbols = true;
+            break;
+        }
+
+        else if (symbolsIn == 'N') {
+            symbols = false;
+            break;
+        }
+        else {
+            std::cout << "Invalid Input";
+        }
+
+    }
+
     char board[8][8];
-    chess chess(true);
+    chess chess(symbols, board);
+
+
     chess.initializeBoard(board);
-    chess.printBoard(board);
+
+    while(true){
+        chess.printBoard(board);
+    }
+
     return 0;
 }
